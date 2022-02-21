d4a3371648b14abc88d42d89fa9b03fdc27dbcf1
julfiker755 <julfiker755.bd@gmail.com>
2022-02-21 07:59:24 -0800

    six commit

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-document.querySelector("#bubb").addEventListener("click",function(event){
-    console.log("Hello bubble parent")
-    event.stopPropagation();
-})
-document.querySelector(".item").addEventListener("click",function(event){
-    console.log("Hello bubble child section")
-    event.stopPropagation();
-})
-document.querySelector("#our").addEventListener("click",function(event){
-    console.log("Ami second child vai-1")
-    event.stopImmediatePropagation();
-})
-document.querySelector("#our").addEventListener("click",function(){
-    console.log("Ami second child vai-2")
-})
-document.querySelector("#our").addEventListener("click",function(){
-    console.log("Ami second child vai-3")
+// var items=document.querySelectorAll(".item")
+// for (const item of items){
+//     item.addEventListener("click",function(event){
+//    // document.getElementById("delegate").removeChild(item)
+//     // event.target.parentNode.removeChild(event.target)
+//     })
+// }
+var btn=document.querySelector("#btn11");
+btn.addEventListener("click",function(){
+    var cre=document.createElement('li');
+    cre.innerHTML="I live in khansama";
+    cre.classList.add('item')
+    var childvai=document.getElementById("delegate");
+     childvai.appendChild(cre)
+});
+document.querySelector("#delegate").addEventListener("click",function(event){
+    event.target.parentNode.removeChild(event.target)
 })
