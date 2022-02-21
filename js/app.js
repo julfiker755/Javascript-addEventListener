b3fc262bd026f056ee8621663584b805e61c9412
julfiker755 <julfiker755.bd@gmail.com>
2022-02-21 07:20:56 -0800

    second commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,8 @@
-
-const demo1=document.querySelector("#button11");
- demo1.addEventListener("click",()=>{
-    document.querySelector("#demo1").innerHTML="Bangladesh";
-    
- })
- const demo2=document.querySelector("#button12");
- demo2.addEventListener("click",function(){
-     document.querySelector("#demo").textContent="I am fine";
- })
- var update=document.querySelector("#update");
- update.addEventListener("click",function(){
-     var text1=document.querySelector("#input-text");
-     var update=document.querySelector(".update-text");
-     update.innerHTML=text1.value;
-     text1.value='';
- })
+const btn=document.querySelector("#button11");
+btn.addEventListener("click",function(){
+const textva=document.querySelector("#comand");
+const newpara=document.createElement("p");
+newpara.innerHTML=textva.value;
+var chi=document.querySelector(".command-area");
+chi.appendChild(newpara)
+})
