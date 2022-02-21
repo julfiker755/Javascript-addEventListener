1ffe68c0d7aa321f4db48622c7bbb595729c8dc9
julfiker755 <julfiker755.bd@gmail.com>
2022-02-21 07:30:01 -0800

    four commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
-// var btn=document.querySelector(".input-text");
-// btn.addEventListener("keyup",function(){
-//     var btn=document.querySelector(".input-text");
-//     console.log(btn.value)
-// })
-// var btn=document.querySelector(".input-text");
-// btn.addEventListener("keydown",function(){
-//     var btn=document.querySelector(".input-text");
-//     console.log(btn.value)
-// })
-var btn=document.querySelector(".input-text");
-btn.addEventListener("keypress",function(){
-    var btn=document.querySelector(".input-text");
-    console.log(btn.value)
+var btn=document.getElementById("button12");
+btn.addEventListener("click",function(){
+ document.querySelector(".delete").style.display='none';
+})
+var btnf=document.querySelector(".input-text");
+btnf.addEventListener("focus",function(){
+ document.body.style.backgroundColor='red';
+})
+btnf.addEventListener("blur",function(){
+    document.body.style.backgroundColor="white";
 })
